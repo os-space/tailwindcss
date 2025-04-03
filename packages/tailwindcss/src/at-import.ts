4b8a842a96a8130eee5d4ce621b4490ab1661551
Jordan Pittman <jordan@cryptica.me>
2025-04-03 13:29:55 -0400

    Track locations inside `@import`

--- packages/tailwindcss/src/at-import.ts
+++ packages/tailwindcss/src/at-import.ts
@@ -10,6 +10,7 @@ export async function substituteAtImports(
   base: string,
   loadStylesheet: LoadStylesheet,
   recurseCount = 0,
+  track = false,
 ) {
   let features = Features.None
   let promises: Promise<void>[] = []
@@ -45,8 +46,8 @@ export async function substituteAtImports(
           }
 
           let loaded = await loadStylesheet(uri, base)
-          let ast = CSS.parse(loaded.content)
-          await substituteAtImports(ast, loaded.base, loadStylesheet, recurseCount + 1)
+          let ast = CSS.parse(loaded.content, { from: track ? uri : undefined })
+          await substituteAtImports(ast, loaded.base, loadStylesheet, recurseCount + 1, track)
 
           contextNode.nodes = buildImportNodes(
             [context({ base: loaded.base }, ast)],
